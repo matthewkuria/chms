be504184c3a7bcdbf89d579c842c8357bcfde786
Matthewkuria <mathewkwachira@gmail.com>
2024-12-30 23:59:13 +0300

    set the page.jsx API endpoint

--- app/page.jsx
+++ app/page.jsx
@@ -20,7 +20,7 @@ export default  function Login() {
     setLoading(true)
     setError('')
   try {
-    const response = await fetch('http://localhost:8000/api/accounts/login/', {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/accounts/login/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',

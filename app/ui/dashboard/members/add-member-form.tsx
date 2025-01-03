8199cb9d25dc1107f69dbfdadd6b9eb71cf087b9
Matthewkuria <mathewkwachira@gmail.com>
2025-01-04 01:46:49 +0300

    Change the add member API endpoint from localhost one

--- app/ui/dashboard/members/add-member-form.tsx
+++ app/ui/dashboard/members/add-member-form.tsx
@@ -58,7 +58,7 @@ export default function AddMemberForm() {
     };
 
     try {
-      const response = await fetch(`http://localhost:8000/api/members/`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/members/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',

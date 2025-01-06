1051c4f90eba4b3c77c92254f581c296c461c9e5
Matthewkuria <mathewkwachira@gmail.com>
2025-01-06 12:51:40 +0300

    Protect all the routes

--- app/dashboard/attendances/page.jsx
+++ app/dashboard/attendances/page.jsx
@@ -1,5 +1,13 @@
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 import AttendanceTable from "../../components/AttendanceTable"
 export default function Page() {
+    const router = useRouter();
+  const token = Cookies.get('access_token');
+        if (!token) { 
+          router.push('/');
+          return;
+        }
     return (
         <main className="">
              <h1>Church  Attendance</h1>
--- app/dashboard/members/page.js
+++ app/dashboard/members/page.js
@@ -1,6 +1,14 @@
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 import MemberTable from '../../components/MemberTable';
 
 export default function MembersPage() {
+  const router = useRouter();
+  const token = Cookies.get('access_token');
+        if (!token) { 
+          router.push('/');
+          return;
+        }
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">Church Members</h1>
